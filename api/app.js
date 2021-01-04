901930cfe2466420d02787699d0c090df8a87d81
afraz98 <anthonymfrazier1998@gmail.com>
2021-01-04 08:06:22 -0500

    Express routing

--- api/app.js
+++ api/app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 var cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require('./routes/testAPI');
+var loginRouter = require('./routes/login');
+var courseRouter = require('./routes/courses')
 
 var app = express();
 
@@ -20,11 +20,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/testAPI', testAPIRouter);
+app.use('/login', loginRouter);
+app.use('/coursework', courseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,7 +39,6 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
-});
+  res.sendFile(path.join(__dirname+'/routes/error.html'));});
 
 module.exports = app;
--- api/routes/login.js
+++ api/routes/login.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 router.get('/', function(req, res, next) {
-    res.send('API is working properly');
+    res.sendFile(path.join(__dirname+'/login.html'));
 });
 
 module.exports = router;
